c95ac788e4e21982b958fbdf02562e87fd0e811c
sunghocho <39304933+shcho1904@users.noreply.github.com>
2023-12-10 04:01:03 +0900

    Update one_main.m

--- one_main.m
+++ one_main.m
@@ -15,37 +15,21 @@ for iter_one=1:10000
     model_load_path = 'data/average_delay/data';
     model_load_path = [model_load_path num2str(iter_one)];
     load(model_load_path);
-    % 1:N_cases
+
     %message passing
-%     [alpha, rho, alpha_his, rho_his] ...
-%         = mp_sol(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, N_ITER, 1);
-%     [~, mp_allo_his] = min(alpha_his+rho_his, [], 2);
-%     time_his = zeros(1,N_ITER);
-%     for i = 1:N_ITER
-%         time_his(i) = sum_time(mp_allo_his(:,:,i), t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-%     end
-%     
-%     [mp_time, mp_min_index] = min(time_his);
-%     mp_allo = mp_allo_his(:,:,mp_min_index);
-%     
-%     [ro_alpha, ro_rho] = mp_sol_rsu_only(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, N_ITER, 1);
-%     [ro_argvalue, ro_mp_allo] = min(ro_alpha + ro_rho, [], 2);
-%     ro_mp_time = sum_time(ro_mp_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-%     
+     [alpha, rho, alpha_his, rho_his] ...
+         = mp_sol(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, N_ITER, 1);
+     [~, mp_allo_his] = min(alpha_his+rho_his, [], 2);
+     
+     [mp_time, mp_min_index] = min(time_his);
+     mp_allo = mp_allo_his(:,:,mp_min_index);
+    
+ 
      [rm_allo, rm_time] = RM(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
      [be_allo, be_time] = BE(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
      [my_allo, my_time] = MY(LOC_Veh_x, LOC_Veh_y, LOC_Rsu_x, LOC_Rsu_y, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num, Rsu_Cpu, Veh_max_time, max_Veh);
      [pm_allo, mu] = prim_dual(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, 30);
      pm_time = sum_time(pm_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-    % mp_time
-    % gr_time
-    % eq_time
-    % km_time
-    
-%     mp_max_time = get_max_time(mp_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-%     gr_max_time = get_max_time(gr_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-%     eq_max_time = get_max_time(eq_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-%     km_max_time = get_max_time(km_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
 
     model_save_path = 'data/average_delay/data';
     model_save_path = [model_save_path num2str(iter_one)];
