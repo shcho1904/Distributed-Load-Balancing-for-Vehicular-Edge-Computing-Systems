4b56bb145c94b7c38b497b1b8c157bfb17e11df1
sunghocho <39304933+shcho1904@users.noreply.github.com>
2023-12-10 03:58:57 +0900

    Update one_main.m

--- one_main.m
+++ one_main.m
@@ -9,10 +9,10 @@ addpath('functions/mp_element');
 N_ITER = 30;
 
 not_conv = 0;
-for iter_one=2001:3000
+for iter_one=1:10000
     tic
     clearvars -except N_ITER DAMPING iter_one not_conv max_Veh
-    model_load_path = 'data/fre_change_final_revise/data';
+    model_load_path = 'data/average_delay/data';
     model_load_path = [model_load_path num2str(iter_one)];
     load(model_load_path);
     % 1:N_cases
@@ -32,11 +32,11 @@ for iter_one=2001:3000
 %     [ro_argvalue, ro_mp_allo] = min(ro_alpha + ro_rho, [], 2);
 %     ro_mp_time = sum_time(ro_mp_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
 %     
-%     [eq_allo, eq_time] = eq_load(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
-%     [gr_allo, gr_time] = bb(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
-    [km_allo, km_time] = kmeans_load(LOC_Veh_x, LOC_Veh_y, LOC_Rsu_x, LOC_Rsu_y, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num, Rsu_Cpu, Veh_max_time, max_Veh);
-%     [pm_allo, mu] = prim_dual(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, 30);
-%     pm_time = sum_time(pm_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
+     [rm_allo, rm_time] = RM(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
+     [be_allo, be_time] = BE(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh);
+     [my_allo, my_time] = myopic(LOC_Veh_x, LOC_Veh_y, LOC_Rsu_x, LOC_Rsu_y, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num, Rsu_Cpu, Veh_max_time, max_Veh);
+     [pm_allo, mu] = prim_dual(t_comm, t_comp_local, t_comp_RSU, RSU_Cpu_num, max_Veh, 30);
+     pm_time = sum_time(pm_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
     % mp_time
     % gr_time
     % eq_time
@@ -46,9 +46,8 @@ for iter_one=2001:3000
 %     gr_max_time = get_max_time(gr_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
 %     eq_max_time = get_max_time(eq_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
 %     km_max_time = get_max_time(km_allo, t_comm, t_comp_RSU, t_comp_local, RSU_Cpu_num);
-    clear vehicle_data
-    
-    model_save_path = 'data/fre_change_final_revise/data';
+
+    model_save_path = 'data/average_delay/data';
     model_save_path = [model_save_path num2str(iter_one)];
     save(model_save_path)
     toc
